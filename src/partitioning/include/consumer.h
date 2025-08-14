30f936d863104b633da831e63f308ded6c79afb9
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2025-08-14 22:13:49 +0530

    Broker,Producer and Consumer

---
+++ src/partitioning/include/consumer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "broker.h"
+
+typedef struct {
+    Broker *broker;
+    char *topic_name;
+    int partition_id;
+    int thread_id;
+} ConsumerArgs;
+
+void *consume_messages(void *args);
