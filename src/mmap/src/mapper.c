a3fe69fd7cb0c00315d728d44a13f586ce226c15
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2025-08-15 11:49:29 +0530

    [mmap] Mmap main.c

--- src/mmap/src/mapper.c
+++ src/mmap/src/mapper.c
@@ -17,5 +17,19 @@ void read_log_with_mmap(char *filepath){
     struct stat st;
     fstat(fd,&st);
 
+    size_t length=st.st_size;
+    char *mapped=mmap(NULL,length,PROT_READ,MAP_SHARED,fd,0);
+    if(mapped==MAP_FAILED){
+        perror("map error");
+        close(fd);
+        return;
+    }
+
+    printf("Readfile using mmap:\n");
+
+    fwrite(mapped,1,length,stdout);
+    munmap(mapped,length);
+    close(fd);
+
     return;
 }
